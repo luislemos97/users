2c3576b77992d41e7b9fdf12c6471a1563d854f0
luslemos97 <luislemosm12@gmail.com>
2022-02-28 09:17:23 -0300

    criação estrutura projeto

---
+++ management-users/src/main/java/com/example/users/ManagementUsersApplication.java
@@ -0,0 +1,13 @@
+package com.example.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementUsersApplication.class, args);
+	}
+
+}
