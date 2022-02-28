e4ccbc2da920055ddb58e2ef9f918750976e1b48
luslemos97 <luislemosm12@gmail.com>
2022-02-28 17:11:08 -0300

    remoção injeção de dependência não utilizada

--- src/main/java/com/example/users/ManagementUsersApplication.java
+++ src/main/java/com/example/users/ManagementUsersApplication.java
@@ -1,7 +1,5 @@
 package com.example.users;
 
-import com.example.users.integration.api.AddressApi;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -13,8 +11,4 @@ public class ManagementUsersApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ManagementUsersApplication.class, args);
 	}
-
-	@Autowired
-	private AddressApi addressApi;
-
 }
